e6d5e0e04ebce786ae515caaed8ecc2085a032d0
Hikmat Jafarli <jafarlihi@gmail.com>
2022-10-04 21:15:40 +0400

    Remove react-trello

--- frontend/src/JobBoard.tsx
+++ frontend/src/JobBoard.tsx
@@ -7,32 +7,11 @@ import {
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 import React, { useState } from 'react';
-import Board from 'react-trello';
 
 const { Header, Sider, Content } = Layout;
 
 const JobBoard: React.FC = () => {
   const [collapsed, setCollapsed] = useState(true);
-
-  const data = {
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'Planned Tasks',
-        label: '2/2',
-        cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
-          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-        ]
-      },
-      {
-        id: 'lane2',
-        title: 'Completed',
-        label: '0/0',
-        cards: []
-      }
-    ]
-  }
 
   return (
     <Layout style={{height: '100vh'}}>
@@ -72,7 +51,7 @@ const JobBoard: React.FC = () => {
               minHeight: 280,
           }}
         >
-          <Board data={data} />
+          Content
         </Content>
       </Layout>
     </Layout>
