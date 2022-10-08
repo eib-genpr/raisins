88adc6bfe060ace70c495ed382d1ef72f098c962
Hikmat Jafarli <jafarlihi@gmail.com>
2022-10-08 21:07:22 +0400

    Add Job ViewSet

--- backend/raisins/urls.py
+++ backend/raisins/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from rest_framework import routers
 from raisins.auth import views
 from raisins.candidates.views import CandidateJobStepViewSet, CandidateViewSet
+from raisins.jobs.views import JobViewSet
 from raisins.resume.views import FileUploadView
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
@@ -12,6 +13,7 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'cjs', CandidateJobStepViewSet)
 router.register(r'candidates', CandidateViewSet)
+router.register(r'jobs', JobViewSet)
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
